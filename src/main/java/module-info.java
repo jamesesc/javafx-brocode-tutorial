5573bc979e8a3e391e58ea8b103df1ae270a0403
James Escudero <esucderojames12@gmail.com>
2024-11-23 20:39:35 -0800

    Update Module-Info
    - Stage run works

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.javafx.brocodetutorial {
     requires javafx.fxml;
 
 
-    opens com.javafx.brocodetutorial to javafx.fxml;
-    exports com.javafx.brocodetutorial;
+    opens com.javafx.brocodetutorial.episode01.stages to javafx.fxml;
+    exports com.javafx.brocodetutorial.episode01.stages;
 }
